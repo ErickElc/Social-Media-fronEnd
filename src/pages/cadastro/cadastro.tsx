9cef07ac635a42668c448108056ec1255da02e9c
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-09-06 11:59:39 -0300

    versionFixed

--- src/pages/cadastro/cadastro.tsx
+++ src/pages/cadastro/cadastro.tsx
@@ -10,12 +10,8 @@ import http from "../../api/api";
 export default function Cadastro(){
 
     const navigate = useNavigate();
-<<<<<<< HEAD
 
     const [inputs, setInputs] = useState({
-=======
-    const [formInputs, setFormInputs] = useState({
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
         name: '',
         age: '',
         email: '',
@@ -40,17 +36,10 @@ export default function Cadastro(){
         
         try{
             const registerRequest = await http.post("api/cadastrar",{
-<<<<<<< HEAD
                 name: inputs.name,
                 age: inputs.age,
                 email: inputs.email,
                 password: inputs.password
-=======
-                name: formInputs.name,
-                age: formInputs.age,
-                email: formInputs.email,
-                password: formInputs.password
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
             })
             registerWorked(registerRequest);
         }
@@ -74,74 +63,42 @@ export default function Cadastro(){
                     <TextField 
                         className="Inputs"
                         id="outlined-basic"
-<<<<<<< HEAD
                         value={inputs.name}
-=======
-                        value={formInputs.name}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                         label="Nome" 
                         variant="outlined"
                         required
                         type='text'
-<<<<<<< HEAD
                         onChange={e => setInputs(prev => ({...prev, name: e.target.value}))}
-=======
-                        onChange={e => setFormInputs( prev => ({...prev, name: e.target.value}))}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                     />
                     <TextField 
                         className="Inputs"
                         id="outlined-basic"
-<<<<<<< HEAD
                         value={inputs.age}
-=======
-                        value={formInputs.age}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                         label="Idade" 
                         variant="outlined"
                         required
                         type='number'
-<<<<<<< HEAD
                         onChange={e => setInputs(prev => ({...prev, age: e.target.value}))}
-=======
-                        onChange={e => setFormInputs( prev => ({...prev, age: e.target.value}))}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                     />
                     <TextField 
                         className="Inputs"
                         id="outlined-basic"
                         required
-<<<<<<< HEAD
                         value={inputs.email}
                         label="Email" 
                         variant="outlined"
                         type='email'
                         onChange={e => setInputs(prev => ({...prev, email: e.target.value}))}
-=======
-                        value={formInputs.email}
-                        label="Email" 
-                        variant="outlined"
-                        type='email'
-                        onChange={e => setFormInputs( prev => ({...prev, email: e.target.value}))}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                     />
                     <TextField 
                         className="Inputs"
                         id="outlined-basic"
-<<<<<<< HEAD
                         value={inputs.password}
-=======
-                        value={formInputs.password}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                         label="Senha" 
                         variant="outlined"
                         required
                         type="password"
-<<<<<<< HEAD
                         onChange={(e) => setInputs(prev => ({...prev, password: e.target.value}))}
-=======
-                        onChange={e => setFormInputs( prev => ({...prev, password: e.target.value}))}
->>>>>>> 9fe1009cba7d02385b093a294f68f62ff01931d2
                     />
                     <Button variant="contained" className="buttonLogin" type="submit">Cadastrar</Button>
                     <TextoLink className="textLink">
