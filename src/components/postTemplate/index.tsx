6c7c63143c06bbfb5d21ac9c5629f823981f71e2
ErickElc <ericklucas@id.uff.br>
2023-01-09 19:27:52 -0300

    postTemplate refatorado

--- src/components/postTemplate/index.tsx
+++ src/components/postTemplate/index.tsx
@@ -1,11 +1,18 @@
-import { AvatarPostImage, PostImage, PostSchema, PubliContainer } from "../../styles/components";
-import { useModalContextEditar } from "../../context/modalEditar";
-import { IComments, IPost } from "../../interface/Interface";
-import { Avatar, Button, TextField } from "@mui/material";
-import { getUserLocalStorage } from "../../auth/util";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
+
+// Config
+import { useModalContextEditar } from "../../context/modalEditar";
+import { getUserLocalStorage } from "../../auth/util";
+import {  IPost } from "../../interface/Interface";
 import http from "../../api/api";
+import * as S from "./styles";
+
+//libs
+import { Avatar, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+
 
 export default function PostTemplate(){
     const modalContext2 = useModalContextEditar();
@@ -41,9 +48,9 @@ export default function PostTemplate(){
     }
     return(
         <div>
-            <PubliContainer>
+            <S.PubliContainer>
                 {postData?.map((item: IPost, index) => (
-                    <PostSchema key={item?._id} onChange={() => {setId(index)}}>
+                    <S.PostSchema key={item?._id} onChange={() => {setId(index)}}>
                         <div className="flex-row  justify-end content-end mb-1">
                             <div className="flex-row items-end content-end justify-end my-3">
                                 {(item?.autor?._id === User._id) ? 
@@ -54,12 +61,15 @@ export default function PostTemplate(){
                                 <Button variant='outlined' onClick={() => toogleMode(item._id)} > Editar </Button> : ''}
                             </div>
                             <Link to={'/perfil/' + item?.autor?._id}>
-                                <div className="flex">
-                                    {(item?.autor?.avatar) ?<AvatarPostImage src={item?.autor?.avatar} alt="/broken-image.jpg" className="mr-3"/>
+                             <div className="flex">
+                                    {(item?.autor?.avatar) ?<S.AvatarPostImage src={item?.autor?.avatar} alt="/broken-image.jpg" className="mr-3"/>
                                     : <Avatar src="/broken-image.jpg" className="mr-3" style={{height: '40px', width: '40px'}}/>
                                     }
                                     <p className="flex items-center">{item?.autor?.name}</p>
                                 </div>
+
+
+                                
                             </Link>
                         </div>
                         <div className="mt-3 mb-3">
@@ -67,12 +77,12 @@ export default function PostTemplate(){
                         </div>
                         <div>
                             {(item?.image_url) ? (
-                                <PostImage src={item?.image_url} alt={item?.content} className="self-center"/>
+                                <S.PostImage src={item?.image_url} alt={item?.content} className="self-center"/>
                             ) : ''}
                         </div>
-                    </PostSchema>   
+                    </S.PostSchema>   
                 ))}
-            </PubliContainer>
+            </S.PubliContainer>
         </div>
     )
 }
