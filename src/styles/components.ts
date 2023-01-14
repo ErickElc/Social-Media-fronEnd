ea13b71e79f8d06f0a78adbcec4ca2a114eafcc0
ErickElc <ericklucas@id.uff.br>
2023-01-14 12:51:36 -0300

    Components

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -1,17 +1,5 @@
 import styled from 'styled-components';
-export const DivBody = styled.div`
-    display:flex;
-    height: 100vh;
-    /* overflow-y: hidden; */
-    padding: 30px;
-    @media (max-height: 600px) {
-        justify-content: flex-end;
-        padding-top: 200px;
-    }
-    justify-content: center;
-    overflow-x: hidden;
-    align-items: center;
-`;
+
 export const HeaderComponent = styled.header`
     top:0px;
     color: white;
@@ -45,33 +33,8 @@ export const StyleForm = {
     display: 'flex',
     flexDirection: 'column'
 };
-export const DivBord = styled.form`
-    background-color: white;
-    box-shadow: 0px 1px 5px 0px #676767;
-    -moz-box-shadow: 0px 1px 5px 0px #676767;
-    -webkit-box-shadow: 0px 1px 5px 0px #676767;
-    display:flex;
-    flex-flow: column;
-    align-items: center;
-    border-radius: 10px;
-    padding: 30px;
-    
 
-`;
-export const TextLogin = styled.p`
-    margin-bottom: 15px;
-    text-align: center;
-    font-size: 2.5rem;
-    font-weight: 700;
 
-`;
-export const TextoLink = styled.p`
-    color: #0096FF;
-    text-decoration: underline;
-    text-align: start;
-    margin-top: 10px;
-
-`;
 export const ModalHeader = styled.div`
     color: white;
     display:flex;
