92fb141eba53194e163bb8a1af2b4595e9af49e4
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-18 17:23:46 -0300

    Config interfaces

--- src/interface/Interface.ts
+++ src/interface/Interface.ts
@@ -1,67 +1,73 @@
 import { SetStateAction } from "react";
 
-export interface IDataContext{
+export interface IDataContext {
     userData?: IData;
     setUserData: React.Dispatch<SetStateAction<IData | undefined>>;
 }
+export interface IDataContext2 {
+    dataContext: {
+        userData?: IData | IPerfil | undefined;
+        setUserData?: React.Dispatch<SetStateAction<IData | undefined>>;
+    };
+}
 export interface IModalState {
-    open?: boolean
+    open?: boolean;
 }
 export interface IModalState2 {
     open?: boolean;
     id: string;
     setId: React.Dispatch<SetStateAction<string>>;
 }
-export interface IContextModal extends IModalState{
+export interface IContextModal extends IModalState {
     openModal: () => void;
     modalState: IModalState;
 }
-export interface IContextModal2 extends IModalState2{
+export interface IContextModal2 extends IModalState2 {
     openModal: () => void;
     modalState: IModalState;
 }
-export interface IPost{
-   content: string,
-   createdDate: string
-    _v: number, 
-    _id: string,
-    image_url: string,
-    autor: IData
+export interface IPost {
+    content: string;
+    createdDate: string;
+    _v: number;
+    _id: string;
+    image_url: string;
+    autor: IData;
 }
-export interface IComments{ 
-    _id: string
-    content: string,
-    autorId: IData,
-    postId: IPost,
-    createdDate: string
+export interface IComments {
+    _id: string;
+    content: string;
+    autorId: IData;
+    postId: IPost;
+    createdDate: string;
 }
-export interface IPost2{
-    content: string,
-    createdDate: string,
-    autor: IData,
-    _v: number, 
-    _id: string
-    Ipost2: () => IPost[] | null
+export interface IPost2 {
+    content: string;
+    createdDate: string;
+    autor: IData;
+    _v: number;
+    _id: string;
+    Ipost2: () => IPost[] | null;
 }
-export interface IPerfil{
-    _id: string,
-    name: string,
-    age: number
-    email: string,
-    createdDate: string ,
-    avatar?: string,
-    status: Number
+export interface IPerfil {
+    _id: string;
+    name: string;
+    age: number;
+    email: string;
+    createdDate: string;
+    avatar?: string;
+    status: Number;
 }
-export interface IData{
-    _id?: string,
-    name?: string,
-    age?: number,
-    cpf?: string,
-    email: string,
-    habilitado?: boolean,
-    avatar?: string,
-    createdDate?: string,
+export interface IData {
+    _id?: string;
+    name?: string;
+    age?: number;
+    cpf?: string;
+    email: string;
+    habilitado?: boolean;
+    avatar?: string;
+    createdDate?: string;
 }
-export interface IPerfis{
-    users: Array<IData>
+export interface IPerfis {
+    users: Array<IData>;
 }
