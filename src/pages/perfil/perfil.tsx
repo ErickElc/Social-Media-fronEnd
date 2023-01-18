58625764d2a4b524e3842eb18144aa8146da5596
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-18 17:24:44 -0300

    Refactoring the perfil screen

--- src/pages/perfil/perfil.tsx
+++ src/pages/perfil/perfil.tsx
@@ -1,111 +1,84 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 //Config
-import { PrivateRoute } from '../../components/protectedLayout/protectedLayout';
-import { useModalContextEditar } from '../../context/modalEditar';
-import { IPerfil, IPost} from '../../interface/Interface';
-import { getUserLocalStorage } from '../../auth/util';
-import http from '../../api/api';
-import '../../styles/index.css'
-import * as S from './styles';
+import { PrivateRoute } from "../../components/protectedLayout/protectedLayout";
+import { useModalContextEditar } from "../../context/modalEditar";
+import { IData, IPost } from "../../interface/Interface";
+import { getUserLocalStorage } from "../../auth/util";
+import http from "../../api/api";
+import "../../styles/index.css";
+import * as S from "./styles";
 
 // Libs
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Avatar, Button } from '@mui/material';
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 //Componets
-import ModalEditar from '../../components/modal/modalEditar';
+import ModalEditar from "../../components/modal/modalEditar";
+import FeedComponent from "../../components/FeedComponent";
+import PostTemplate from "../../components/postTemplate";
 
-export default function Perfil(){
-    const modalContext2 = useModalContextEditar();
-    const {id} = useParams();
-    const route = `/conta/${id}`;
-    const [data, setData] = useState<IPerfil>();
-    const user = getUserLocalStorage();
-    const [postsPerfil, setPostsPerfil] = useState([]);
+export default function Perfil() {
+    const { id } = useParams();
     const navigate = useNavigate();
-    useEffect(()=>{
-        http.get(`api/users/${id}`).then((res)=>{
-            if(res.data !== null){
-                setData(res.data)
-            }
-        }).catch(err => {
-            console.log(err)
-            setData(err.response.status);
-            navigate('/naoexiste')
-        })
-    },[])
-    useEffect(()=>{
-        http.get(`api/posts/list/user/${id}`).then((res)=> {
-            setPostsPerfil(res.data);
-        }).catch(err => {
-            console.log(err)
-        })
-    },[])
+    const user = getUserLocalStorage();
+    const [data, setData] = useState<IData>();
+    const [postData, setPostsData] = useState([]);
+    const modalContext2 = useModalContextEditar();
+
+    useEffect(() => {
+        http.get(`api/users/${id}`)
+            .then((res) => {
+                if (res.data !== null) {
+                    setData(res.data);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                setData(err.response.status);
+                navigate("/naoexiste");
+            });
+    }, []);
+
+    useEffect(() => {
+        http.get(`api/posts/list/user/${id}`)
+            .then((res) => {
+                setPostsData(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     const deletePost = async (id: String | undefined) => {
         try {
-            const response = await http.post(`api/posts/delete/${id}`,{
-              token: user?.token,
-              email: user?.email
-            })
-            if(response.status){
-                alert('Post Excluido com sucesso');
+            const response = await http.post(`api/posts/delete/${id}`, {
+                token: user?.token,
+                email: user?.email
+            });
+            if (response.status) {
+                alert("Post Excluído com sucesso");
                 window.location.reload();
             }
-          
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-    }
+    };
+
     const toogleMode = (id: string) => {
-        modalContext2?.setId(id)
+        modalContext2?.setId(id);
         modalContext2.openModal();
-    }
-    return(
+    };
+
+    return (
         <PrivateRoute>
-            <S.SectionContainer> 
+            <S.SectionContainer>
+                <FeedComponent dataContext={{ userData: data, setUserData: setData }} />
                 <div>
-                    <S.FeedComponent>
-                        {(data?.avatar) ? <S.AvatarImage src={data?.avatar} alt="/broken-image.jpg" className=""/>
-                        : <Avatar src="/broken-image.jpg" className="mr-3" style={{height: '70px', width: '70px'}}/>
-                        }
-                        <p className='mb-3 text-white'>{data?.name}</p>
-                        {
-                            user?._id === data?._id ? <Link to={route}><Button variant="contained">Editar Perfil</Button></Link> : ''
-                        }
-                    </S.FeedComponent>
+                    <h2 className="font-bold text-center mb-4">Ultimos posts: </h2>
+                    <PostTemplate deletePost={deletePost} toggleMode={toogleMode} postData={postData} />
                 </div>
-                <div>
-                    <h2 className='font-bold text-center mb-4'>Ultimos posts: </h2>
-                    <S.PubliContainer>
-                        {postsPerfil.map((item: IPost) => (
-                            <S.PostSchema key={item?._id}>
-                                {(item?.autor?._id === user._id) ? 
-                                <Button  style={{width: 100, margin: 10}}variant='contained' onClick={() => deletePost(item?._id)} > Deletar</Button> : ''}
-                                {(item?.autor?._id === user._id) ? 
-                                <Button variant='outlined' style={{width: 100, margin: 10}} onClick={() => toogleMode(item._id)} > Editar </Button> : ''}
-                                <S.ContainerDivAutor>
-                                    {(item?.autor?.avatar) 
-                                    ? <S.AvatarPostImage src={item?.autor?.avatar} alt="/broken-image.jpg" className=""/>
-                                    : <Avatar src="/broken-image.jpg" className="" style={{height: '40px', width: '40px'}}/>
-                                    }
-                                    <p className="flex-row  items-center ml-3">{item?.autor.name}</p>
-                                </S.ContainerDivAutor>
-                                <div className="mb-3">
-                                    <p>{item?.content}</p>
-                                </div>
-                                <div>
-                                    {(item?.image_url) ? <S.PostImage src={item?.image_url} alt={item?.content} className="self-center"/> : ''}
-                                </div>
-                            </S.PostSchema>   
-                        ))}
-                    </S.PubliContainer>
-                </div>
-                <ModalEditar/>
+                <ModalEditar />
             </S.SectionContainer>
         </PrivateRoute>
-
-    )
-
-
+    );
 }
--- src/pages/perfil/styles.ts
+++ src/pages/perfil/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const AvatarImage = styled.img`
     width: 80px;
@@ -6,28 +6,28 @@ export const AvatarImage = styled.img`
     max-height: 80px;
     max-width: 80px;
     border-radius: 50%;
-`
+`;
 export const FeedComponent = styled.div`
-background: rgb(141,5,240);
-background: linear-gradient(90deg, rgba(141,5,240,1) 0%, rgba(79,0,176,1) 35%, rgba(103,0,119,1) 100%);
-padding: 50px;
-border-radius: 10px;
-display: flex;
-flex-flow: column;
-justify-content: center;
-align-items: center;
--moz-box-shadow: 0px 1px 5px 0px #676767;
--webkit-box-shadow: 0px 1px 5px 0px #676767;
-box-shadow: 0px 1px 5px 0px #676767;
-justify-self: center;
-margin-bottom: 10px;
-width: 580px;
-@media (max-width: 600px) {
-    width: 340px
-}
-@media (max-width: 380px) {
-    width: 270px
-}
+    background: rgb(141, 5, 240);
+    background: linear-gradient(90deg, rgba(141, 5, 240, 1) 0%, rgba(79, 0, 176, 1) 35%, rgba(103, 0, 119, 1) 100%);
+    padding: 50px;
+    border-radius: 10px;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    -moz-box-shadow: 0px 1px 5px 0px #676767;
+    -webkit-box-shadow: 0px 1px 5px 0px #676767;
+    box-shadow: 0px 1px 5px 0px #676767;
+    justify-self: center;
+    margin-bottom: 10px;
+    width: 580px;
+    @media (max-width: 600px) {
+        width: 340px;
+    }
+    @media (max-width: 380px) {
+        width: 270px;
+    }
 `;
 
 export const PostSchema = styled.div`
@@ -52,36 +52,40 @@ export const PostSchema = styled.div`
         -webkit-box-shadow: 0px 0px 0px 0px #676767;
         padding: 0px;
     }
-    @media (max-width: 380px){
+    @media (max-width: 380px) {
         width: 270px;
     }
-`
+`;
 export const PubliContainer = styled.div`
     display: flex;
     flex-flow: column-reverse;
     justify-content: center;
     align-items: center;
-`
+`;
 export const SectionContainer = styled.main`
-    display:flex;
+    display: flex;
     flex-flow: column wrap;
     justify-content: center;
     align-items: center;
-`
+    padding: 40px;
+    @media (max-width: 450px) {
+        padding: 20px;
+    }
+`;
 export const ContainerDivAutor = styled.div`
     display: flex;
     flex-flow: row wrap;
     align-items: center;
     justify-content: flex-start;
     margin-bottom: 20px;
-`
+`;
 export const AvatarPostImage = styled.img`
     width: 50px;
     height: 50px;
     max-width: 50px;
     max-height: 50px;
     border-radius: 50%;
-`
+`;
 export const PostImage = styled.img`
     height: 300px;
     width: 470px;
@@ -93,4 +97,4 @@ export const PostImage = styled.img`
     @media (max-width: 400px) {
         max-width: 270px;
     }
-`
+`;
