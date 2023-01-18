c244f4046e14be2890702edd2d7e4fa5462c2e22
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-18 17:22:07 -0300

    selected api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const apis = ['http://localhost:4000/', 'https://social-media-senai.herokuapp.com/', 'https://tktyxxvgvd.us-east-1.awsapprunner.com/']
+const apis = ['http://localhost:4000/', 'https://social-media-senai.herokuapp.com/', 'https://socialmediabackendv2-production.up.railway.app']
 
 const http = axios.create({
-    baseURL: apis[0]
+    baseURL: apis[2]
 })
 
 
