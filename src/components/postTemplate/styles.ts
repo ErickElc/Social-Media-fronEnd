e1cc32bc2ddaa55d23c23a1ba250ad2d5daf6b87
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-18 17:23:29 -0300

    Post Template

--- src/components/postTemplate/styles.ts
+++ src/components/postTemplate/styles.ts
@@ -1,70 +1,61 @@
 import styled from "styled-components";
 
-
-
 export const PubliContainer = styled.div`
     display: flex;
     flex-flow: column-reverse;
     justify-content: center;
     align-items: center;
-`
+    padding-bottom: 30px;
+`;
 export const PostSchema = styled.div`
-    background-color: whitesmoke;
     -moz-box-shadow: 0px 1px 5px 0px #676767;
     -webkit-box-shadow: 0px 1px 5px 0px #676767;
     box-shadow: 0px 2px 6px 1px #676767;
-    padding: 50px;
+    padding: 40px;
+    max-width: 880px;
+    width: 100%;
+    border-radius: 14px;
+`;
+export const PostTop = styled.div`
     display: flex;
-    flex-flow: wrap column;
+    flex-flow: row wrap;
+    align-items: center;
     justify-content: space-between;
-    word-break: break-all;
-    border-radius: 10px;
-    margin-bottom: 30px;
-    width: 580px;
-    max-width: 580px;
-    min-height: 40vh;
-    @media (max-width: 600px) {
-        width: 340px;
-        box-shadow: 0px 0px 0px 0px #676767;
-        -moz-box-shadow: 0px 0px 0px 0px #676767;
-        -webkit-box-shadow: 0px 0px 0px 0px #676767;
-        padding: 0px;
-    }
-    @media (max-width: 380px){
-        width: 270px;
-    }
-`
+`;
 
+export const AutorText = styled.h2`
+    color: black;
+    font-size: 24px;
+    font-weight: 500;
+    font-family: Arial, Helvetica, sans-serif;
+    @media (max-width: 450px) {
+        font-size: 17px;
+    }
+`;
 export const PostImage = styled.img`
-    height: 300px;
-    width: 470px;
-    align-self: center;
-    text-align: center;
-    @media (max-width: 600px) {
-        max-width: 340px;
-    }
-    @media (max-width: 400px) {
-        max-width: 270px;
-    }
-`
+    width: 100%;
+    height: 100%;
+`;
 
 export const AvatarPostImage = styled.img`
     width: 50px;
     height: 50px;
-    max-width: 50px;
-    max-height: 50px;
-    border-radius: 50%;
-`
-export const ContainerDivAutor = styled.div`
-    display: flex;
-    flex-flow: row wrap;
-    align-items: center;
-    justify-content: flex-start;
-    margin-bottom: 20px;
-`
+    @media (max-width: 450px) {
+        width: 40px;
+        height: 40px;
+    }
+`;
 
-export const DivContainer = styled.div`
-    display:flex;
-    flex-flow: column wrap;
-    align-items: center;
-`
+export const ContentPost = styled.p`
+    text-align: justify;
+    max-width: 100%;
+    font-size: 26px;
+    color: black;
+    @media (max-width: 450px) {
+        font-size: 16px;
+    }
+`;
+
+export const ContainerDivAutor = styled.div``;
+
+export const DivContainer = styled.div``;
