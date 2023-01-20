26323a4e6ca3175a196237b2cb545316818d7260
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-01-20 17:49:25 -0300

    AuthContext refatorado

--- src/auth/interfaces.ts
+++ src/auth/interfaces.ts
@@ -1,18 +1,15 @@
-
-export interface IUser{
-    email?: string,
-    token?: string,
-    _id?: string,
+export interface IUser {
+    email?: string;
+    token?: string;
+    _id?: string;
 }
 
-
-export interface IContext extends IUser{
+export interface IContext extends IUser {
     authenticate: (email: string, password: string) => Promise<number | undefined>;
     logout: () => void;
-    VerifyLoggin: () => void
+    VerifyLoggin: () => void;
 }
 
-
-export interface IAuthProvider{
+export interface IAuthProvider {
     children: JSX.Element[] | JSX.Element;
 }
--- src/auth/util.ts
+++ src/auth/util.ts
@@ -1,27 +1,26 @@
-import http from "../api/api"
 import { IData } from "../interface/Interface";
+import http from "../api/api";
 
+export const setUserLocalStorage = (user: IData | null) => {
+    localStorage.setItem("u", JSON.stringify(user));
+};
 
-
-export const setUserLocalStorage = (user: IData | null) =>{
-    localStorage.setItem('u', JSON.stringify(user));
-}
 export const getUserLocalStorage = () => {
-    const json = localStorage.getItem('u')
-    if(!json) return null;
-
+    const json = localStorage.getItem("u");
+    if (!json) return null;
 
     const user = JSON.parse(json);
 
     return user ?? null;
-}
-export async function LoginRequest(email: string, password: string){
+};
+
+export const LoginRequest = async (email: string, password: string) => {
     try {
-        const request = await http.post('api/users/login',{email, password});
-        const dataRequest = await http.post('api/users/list/email',{email: email, token: request.data});
-        console.log(dataRequest.data)
-        return {request: request, dataRequest: dataRequest.data};
+        const request = await http.post("api/users/login", { email, password });
+        const dataRequest = await http.post("api/users/list/email", { email: email, token: request.data });
+        console.log(dataRequest.data);
+        return { request: request, dataRequest: dataRequest.data };
     } catch (error) {
-       return null 
+        return null;
     }
-}
+};
