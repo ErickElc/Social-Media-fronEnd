20d49a2ab36d7b9b5cc37ab7d1a213a55f06031b
ErickElc <ericklucas@id.uff.br>
2023-01-20 20:51:52 -0300

    ....

--- save/src/auth/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { AuthContext } from "./Auth";
-
-export const useAuth = () => {
-    const context = useContext(AuthContext);
-
-    return context;
-};
--- save/src/components/PostTemplate/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import { IPost } from "../../interface/Interface";
-
-export interface IPostTemplate {
-    postData: IPost[];
-    deletePost: (id: String) => Promise<void>;
-    toggleMode: (id: string) => void;
-}
